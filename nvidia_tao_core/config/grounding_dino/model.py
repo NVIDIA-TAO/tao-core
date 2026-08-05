55a96be77d9807b20fc736e5b3bbac69730bfaff
Ramanathan Arunachalam <rarunachalam@nvidia.com>
2026-08-05 20:02:38 +0000

    fix: complete AutoML parameter bounds
    Signed-off-by: Ramanathan Arunachalam <rarunachalam@nvidia.com>

--- nvidia_tao_core/config/grounding_dino/model.py
+++ nvidia_tao_core/config/grounding_dino/model.py
@@ -294,6 +294,7 @@ class GDINOModelConfig:
         default_value=6,
         description="Numer of encoder layers in the transformer",
         valid_min=1,
+        valid_max=12,
         automl_enabled="TRUE",
         display_name="encoder layers",
     )
@@ -302,6 +303,7 @@ class GDINOModelConfig:
         default_value=6,
         description="Numer of decoder layers in the transformer",
         valid_min=1,
+        valid_max=12,
         automl_enabled="TRUE",
         display_name="decoder layers",
     )
